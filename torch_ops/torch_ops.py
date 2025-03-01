70cb94efa9c3f9092dfc0656e73ac0d727a782eb
MohammadReza Omrani <52138433+mohammad-reza-omrani@users.noreply.github.com>
2025-03-01 16:24:23 +0100

    Create torch_ops.py

---
+++ torch_ops/torch_ops.py
@@ -0,0 +1,11 @@
+import torch  # Import the torch library
+
+# Create two tensors filled with ones of size (1, 2), and add them together
+tensor1 = torch.ones(1, 2)
+tensor2 = torch.ones(1, 2)
+result = tensor1 + tensor2
+
+# Print the result
+print(tensor1)
+print(tensor2)
+print(result)
